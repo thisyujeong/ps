e25d41f9032a7222c69a1851dacb2db0822efe69
thisyujeong <this.yujeong@gmail.com>
2023-12-12 17:47:42 +0900

    Add: BOJ 2667 단지번호붙이기 BFS 풀이 추가

--- baekjoon/2667_단지번호붙이기.js
+++ baekjoon/2667_단지번호붙이기.js
@@ -1,13 +1,12 @@
 /**
- * 집이 있는 좌표는 1로 표시되므로,
- * 방문한 집은 0으로 처리하고 1일 경우 방문하지 않은 집으로 간주
- *
+ * 집이 있는 좌표는 1로 표시되므로, 방문한 집은 0으로 처리하고 1일 경우 방문하지 않은 집으로 간주
+ * DFS와 BFS 방식을 활용할 수 있음
  */
 const input = require('fs').readFileSync('./input.txt').toString().split('\n');
 const n = +input.shift();
 const count = []; // 각 단지에 속하는 집의 수
 const graph = input.map((line) => line.split('').map(Number));
-let home = 0;
+let home = 0; // 한 단지의 집 수 카운트
 
 // 상하좌우 탐색을 위한 좌표
 const dx = [-1, 1, 0, 0];
@@ -15,8 +14,10 @@ const dy = [0, 0, -1, 1];
 
 for (let i = 0; i < n; i++) {
   for (let j = 0; j < n; j++) {
+    // 해당 좌표를 방문한 적이 없다면 수행
     if (graph[i][j] === 1) {
-      dfs(i, j);
+      bfs(i, j);
+      // dfs 또는 bfs를 빠져나왔다면 단지 하나를 모두 탐색한 것
       count.push(home);
       home = 0;
     }
@@ -27,7 +28,7 @@ count.sort((a, b) => a - b);
 console.log(count.length);
 count.forEach((cnt) => console.log(cnt));
 
-// dfs
+// 1. dfs 깊이우선탐색
 function dfs(x, y) {
   // 해당 좌표가 범위내에 있고 방문한 적이 없다면 수행
   if (rangeCheck(x, y) && graph[x][y] === 1) {
@@ -38,6 +39,25 @@ function dfs(x, y) {
     // 상하좌우 탐색
     for (let k = 0; k < dx.length; k++) {
       dfs(x + dx[k], y + dy[k]);
+    }
+  }
+}
+
+// 2. bfs 너비우선탐색
+function bfs(x, y) {
+  const queue = [[x, y]];
+  graph[x][y] = 0; // 방문처리
+
+  while (queue.length) {
+    const [cx, cy] = queue.shift();
+    home += 1;
+
+    for (let k = 0; k < dx.length; k++) {
+      const [nx, ny] = [cx + dx[k], cy + dy[k]];
+      if (rangeCheck(nx, ny) && graph[nx][ny] === 1) {
+        queue.push([nx, ny]);
+        graph[nx][ny] = 0;
+      }
     }
   }
 }
